44ab6bc6890380298ac4e6fbe36b1af8c7a4334a
fahminlb33 <fahminlb33@gmail.com>
2021-12-12 11:51:42 +0700

    Add unit test

--- infrastructure/authentication/password_test.go
+++ infrastructure/authentication/password_test.go
@@ -7,6 +7,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestHashPassword(t *testing.T) {
+	result, _ := authentication.HashPassword("fahmi")
+	assert.Contains(t, result, "$2a$14$") // make sure to have 14 rounds
+}
+
 func TestVerifyPassword(t *testing.T) {
 	result := authentication.VerifyPassword("fahmi", "$2a$14$FwEMlETO/XHoB90v/O9zK.KNfm.G5ZxJUcoZWS5IFBOM/Ao4adKiW")
 	assert.True(t, result)
